120752bcfd2476ac06177f9fcaf6b247b122e91a
zhanghao.gino <zhanghao.gino@bytedance.com>
2023-01-18 15:57:02 +0800

    react 17 与 3.5 冲突
    Change-Id: I52e24808a2a15754c6342bce5c72bdf526162378

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,8 @@
 import { Component, PropsWithChildren } from "react";
-import { View, Text, Form, Label } from "@tarojs/components";
+import { View, Text, Form, Label, Input, Map } from "@tarojs/components";
 import "./index.less";
 import TestCP from "../../components/TestCP";
 import TestCP2 from "../../components/TestCP2";
-
 
 export default class Index extends Component<PropsWithChildren> {
   componentWillMount() {}
@@ -21,8 +20,11 @@ export default class Index extends Component<PropsWithChildren> {
       <View className='index'>
         <Text>Hello world!</Text>
         <Form>
-          <Label>测试</Label>
+          <Label>内部测试</Label>
         </Form>
+        <Label>外部</Label>
+        <Input />
+        <Map latitude={40.22077} longitude={116.23128} />
         <TestCP></TestCP>
         <TestCP2></TestCP2>
       </View>
